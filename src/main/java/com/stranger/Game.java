69b067807cca93555da95516d2fe963be44e4fb6
davidokun <davidokun99@gmail.com>
2018-08-03 17:10:53 -0500

    Some intermediate stuff

--- src/main/java/com/stranger/Game.java
+++ src/main/java/com/stranger/Game.java
@@ -1,5 +1,6 @@
 package com.stranger;
 
+import com.stranger.util.ConsoleColor;
 import com.stranger.util.LoadAsset;
 
 import java.io.IOException;
@@ -16,11 +17,18 @@ public class Game {
     public static void main(String[] args) {
 
         LoadAsset loadAsset = new LoadAsset();
-        Optional<Path> title = loadAsset.loadAsset("title.txt");
+        Optional<Path> title = loadAsset.loadAsset("stranger-stuff/title.txt");
 
         if (title.isPresent()) {
             try (Stream<String> stream = Files.lines(title.get())) {
-                stream.forEach(System.out::println);
+                stream.forEach(l -> {
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(ConsoleColor.RED + l + ConsoleColor.RESET);
+                });
             } catch (IOException e) {
                 e.printStackTrace();
             }
