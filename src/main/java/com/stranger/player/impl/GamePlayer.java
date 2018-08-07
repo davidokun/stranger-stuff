bc47610f867e0ffc0e375c958b2af025ccb54d0d
davidokun <davidokun99@gmail.com>
2018-08-06 20:29:53 -0500

    Rename player implementation - GamePlayer

---
+++ src/main/java/com/stranger/player/impl/GamePlayer.java
@@ -0,0 +1,14 @@
+package com.stranger.player.impl;
+
+import com.stranger.player.Player;
+
+public class GamePlayer extends Player {
+
+    public GamePlayer() {}
+
+    public GamePlayer(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
