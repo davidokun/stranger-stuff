e6a68fe36e56b61272801cfcdfd09ff07c5bf3ca
davidokun <davidokun99@gmail.com>
2018-08-09 17:26:13 -0500

    Cleaning the Main class and using the GameDirector

--- src/main/java/com/stranger/Game.java
+++ src/main/java/com/stranger/Game.java
@@ -1,14 +1,5 @@
 package com.stranger;
 
-import com.stranger.util.ConsoleColor;
-import com.stranger.util.LoadAsset;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Optional;
-import java.util.stream.Stream;
-
 /**
  * This is the initial point of access of the game
  */
@@ -16,22 +7,8 @@ public class Game {
 
     public static void main(String[] args) {
 
-        LoadAsset loadAsset = new LoadAsset();
-        Optional<Path> title = loadAsset.loadAsset("stranger-stuff/title.txt");
+        GameDirector gameDirector = new GameDirector();
+        gameDirector.gameFlow();
 
-        if (title.isPresent()) {
-            try (Stream<String> stream = Files.lines(title.get())) {
-                stream.forEach(l -> {
-                    try {
-                        Thread.sleep(200);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println(ConsoleColor.RED + l + ConsoleColor.RESET);
-                });
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
